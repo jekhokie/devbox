5fbe6e7adab861f00daed1797e0b1673efef92b5
Justin Karimi <jekhokie@gmail.com>
2016-08-01 07:03:22 -0400

    Add Package Management and Simplify Code
    Simplify the code base for variables and add the ability to specify
    default packages to include in the build (including a script to install
    the packages).

--- create_vms.py
+++ create_vms.py
@@ -18,28 +18,43 @@ import shlex
 config = Config(file('config/settings.ini'))
 
 # construct variables based on the configuration
-os_name = config.os_name
-os_version = config.os_version
-memory = config.vm_memory
-num_cpus = config.vm_num_cpus
-homedir_files = config.homedir_files
-packer_dir = "packer/{}{}".format(os_name, os_version)
+packer_dir = "packer/{}{}".format(config.os_name, config.os_version)
 
 # run the Packer command to build the VM based on the inputs
-packer_command = shlex.split("packer build -var 'homedir_files={}' -var 'memory={}' -var 'num_cpus={}' base_template.json".format(homedir_files, memory, num_cpus))
-call(packer_command, cwd=packer_dir)
+packer_string = '''
+packer build -var 'apt_packages="{}"'
+             -var 'homedir_files={}'
+             -var 'memory={}'
+             -var 'num_cpus={}'
+             base_template.json
+'''.format(config.apt_packages, config.homedir_files, config.vm_memory, config.vm_num_cpus)
+call(shlex.split(packer_string), cwd=packer_dir)
 
 # next steps if the build succeeded
 print '''
-##############################################################################
-# Your artifact(s) have finished building. You can find them in the          #
-# artifacts/ directory with the names shown in the output above. To          #
-# use the artifact (i.e. in Vagrant), run the command below:                 #
-#                                                                            #
-#    vagrant box add --name "packer-ubuntu16" artifacts/<artifact_name>.box  #
-#                                                                            #
-# Where '<artifact_name>' is the name of the Vagrant box file. You           #
-# can then insert the name of your box (i.e. 'packer-ubuntu16') into         #
-# your Vagrantfile to use it.                                                #
-##############################################################################
+###############################################################################################
+# Your artifact(s) have finished building. You can find them in the                           #
+# artifacts/ directory with the names shown in the output above. To                           #
+# use the artifact (i.e. in Vagrant), run the command below:                                  #
+#                                                                                             #
+#    vagrant box add --name "packer-ubuntu16" artifacts/<artifact_name>.box                   #
+#                                                                                             #
+# Where '<artifact_name>' is the name of the Vagrant box file. You                            #
+# can then insert the name of your box (i.e. 'packer-ubuntu16') into                          #
+# your Vagrantfile to use it. Here is a sample Vagrantfile for use once the                   #
+# box has been imported for use using the name "packer-ubuntu16", for example:                #
+#                                                                                             #
+#   -*- mode: ruby -*-                                                                        #
+#   vi: set ft=ruby :                                                                         #
+#   Vagrant.configure(2) do |config|                                                          #
+#       config.vm.box = "packer-ubuntu16"                                                     #
+#                                                                                             #
+#       config.vm.define "test" do |config|                                                   #
+#           config.ssh.username = "vagrant"                                                   #
+#           config.ssh.password = "vagrant"                                                   #
+#           config.vm.network "private_network", ip: "10.11.13.14", netmask: "255.255.255.0"  #
+#       end                                                                                   #
+#   end                                                                                       #
+#                                                                                             #
+###############################################################################################
 '''
--- packer/ubuntu16.04/scripts/home_dir.sh
+++ packer/ubuntu16.04/scripts/home_dir.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Purpose: Inject any home directory contents desired from a GitHub repo.
 
+# install the git package since this script requires it (if not already installed)
+sudo apt-get install git
+
 # clone the repository containing the home directory files
 git clone $homedir_files /home/vagrant/homedir
 
